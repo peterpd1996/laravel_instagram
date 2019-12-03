62c63979152d600bacc877d8eb1704b9a99006c1
vagrant <vagrant@vagrant.vm>
2019-12-03 17:14:22 +0000

    database setup

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host']??null;
+$username = $url['user']??null;
+$password = $url['pass']??null;
+$database = substr($url['path'], 1);
+
+
 return [
 
     /*
@@ -15,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -76,6 +83,17 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+           
         ],
 
         'sqlsrv' => [
