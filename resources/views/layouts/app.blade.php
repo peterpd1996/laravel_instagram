8f6ecbad39f533f7ede7e882ea585bc83cea583b
unknown <tungduongpd1996@gmail.com>
2019-12-05 22:22:33 +0700

    deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,171 +1,103 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-   
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="/images/ins.jpg">
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/jquery.js') }}" ></script>
-    <script src="{{ asset('js/js.js') }}" defer ></script>
-  
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-  
-
-</head>
-<body>
-    <div id="app">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="/images/ins.jpg">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/jquery.js') }}" ></script>
+        <script src="{{ asset('js/js.js') }}" defer ></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+        <!-- Styles -->
+    </head>
+    <body>
+        <div id="app">
         <nav class="navbar navbar-expand-md navbar-light shadow-sm navigation padding">
             <div class="container">
                 <a class="navbar-brand d-flex" style="padding-right: 247px" href="{{ url('/') }}">
-                  <div ><i class="fa fa-instagram pr-3" style="border-right: 1px solid black"></i></div>
-                  <div class="pl-3">Instagram</div>
+                    <div ><i class="fa fa-instagram pr-3" style="border-right: 1px solid black"></i></div>
+                    <div class="pl-3">Instagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
                 </button>
-               
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav " style="padding-right: 250px">
-                            @if (Auth::check())
-                            <div class=""  style="position: relative">
-                                
-                                <input type="text" id="searchText" class="border_" placeholder="             Search" size="23">
-                            
+                        @if (Auth::check())
+                        <div class=""  >
+                            <input type="text" id="searchText" class="border_" placeholder="             Search" size="23">
                             <div class="contais">
                                 <ul class="result " id="resultSearch">
-                                    
                                 </ul>
-        
                             </div>
-                        
-                            </div>
-                            @endif
+                        </div>
+                        @endif
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                        <div style="position: relative;left: 200px" class="d-flex">
+                            <li class="nav-item mr-3">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                         </div>
+                        @endif
                         @else
                         {{-- icon notification, user --}}
-                          <div class="icon-notification d-flex pt-2" style>
-                                <div id="" style="cursor: pointer;position: relative;width: 20px;margin-right: 17px">
-                                        <i class="fa fa-bell-o " aria-hidden="true" id="notify"></i>
-                                        <div id="count"></div> 
-                                        {{-- // count notification --}}
-                                        <ul class="notifi disable" id="notifShow" style="position: absolute">
-                                        </ul>
-
-                                </div>
-                                <div class=" pr-2"><a href="/profile/{{ Auth::user()->id}}">
-                                <i class="fa fa-user-o text-dark" aria-hidden="true"></i>
-                                    
-                              </a>
+                        <div class="icon-notification d-flex pt-2" style>
+                            <div id="" style="cursor: pointer;position: relative;width: 20px;margin-right: 17px">
+                                <i class="fa fa-bell-o " aria-hidden="true" id="notify"></i>
+                                <div id="count"></div>
+                                {{-- // count notification --}}
+                                <ul class="notifi disable" id="notifShow" style="position: absolute">
+                                </ul>
                             </div>
-                          </div>
-                           {{-- end icon notification, user --}}
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->username }} <span class="caret"></span>
+                            <div class=" pr-2"><a href="/profile/{{ Auth::user()->id}}">
+                                <i class="fa fa-user-o text-dark" aria-hidden="true"></i>
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            </div>
+                        </div>
+                        {{-- end icon notification, user --}}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->username }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
-        {{-- <nav class="nav navbar-light shadow-sm" >
-            <div class="container">
-                    <div class="row d-flex align-items-center ">
-                        <div class="col-md-3">
-                                <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                                    <div ><i class="fa fa-instagram pr-3" style="border-right: 1px solid black"></i></div>
-                                    <div class="pl-3">Instagram</div>
-                                  </a>
-                        </div>
-                        <div class="col-md-5">
-                                @if (Auth::check())
-                                    <div class=""  style="position: relative;padding-left: 200px">
-                                        
-                                        <input type="text" id="searchText" class="border_" placeholder="             Search" size="23">
-                                    
-                                    <div class="contais">
-                                        <ul class="result" id="resultSearch">
-                                            
-                                        </ul>
-                
-                                    </div>
-
-                                    </div>
-                                @endif
-                        </div>
-                        <div class="col-md-4 d-flex justify-content-center">
-                                <div><i class="fa fa-bell-o pt-2 pr-4" aria-hidden="true"></i></div>
-                                <div class="pt-2 pr-2"><a href="/profile/{{ Auth::user()->id}}">
-                                    <i class="fa fa-user-o text-dark" aria-hidden="true"></i>
-                                    
-                                </a></div>
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-dark" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->username }} <span class="caret"></span>
-                                    </a>
-    
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-    
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                            </div>
-                    </div>
-            </div>
-        </nav> --}}
 
         <main class="py-4 main">
-            @yield('content')
+        @yield('content')
         </main>
-    </div>
-
-    
-   
-</body>
+        </div>
+    </body>
 </html>
 <script>
     $(document).ready(function(){
@@ -226,13 +158,10 @@
                 }
             })
         }
-
-})
-
-
+    
+    })
+    
+    
     
 </script>
-
-
 @yield('js')
-
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,65 +4,62 @@
     @csrf
     <div class="container">
         <div class="row">
-        
-                <div class="col-10">
-                    <div class="form-group row pb-1">
-                        <label for="caption" class="col-md-4 col-form-label text-md-right">Post Caption</label>
-            
-                        <div class="col-md-8">
-                            <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}"  autocomplete="caption" autofocus>
-            
-                            @error('caption')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="form-group row pb-2">
-                        <label for="image" class="col-md-4 col-form-label text-md-right ">Post Image</label>
-                        <div class="col-md-8" style="position: relative">
-                                <div id="iconUpload"> <i class="fa fa-picture-o" aria-hidden="true"></i> Photo</div>
-                                <input id="uploadNewPost" type="file"  name="image" id="image" class=" @error('image') is-invalid @enderror" autocomplete="image" autofocus>
-                                @error('image')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-                    </div>
-                    <div class="col-md-8 offset-md-4">
-                        <img id="img_output" src="" alt="" width="400px" height="400px">
-                    </div>
-                    <div class="row pl-3 mt-2">
-                    <button class="btn btn-primary col-md-3 offset-md-4 ">Add New Post</button>
+            <div class="col-10">
+                <div class="form-group row pb-1">
+                    <label for="caption" class="col-md-4 col-form-label text-md-right">Post Caption</label>
+                    <div class="col-md-8">
+                        <textarea class="form-control" rows="5" id="caption"  class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}"  autocomplete="caption" autofocus>
+                        </textarea>
+                        @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
-            
+                <div class="form-group row pb-2">
+                    <label for="image" class="col-md-4 col-form-label text-md-right ">Post Image</label>
+                    <div class="col-md-8" style="position: relative">
+                        <div id="iconUpload"> <i class="fa fa-picture-o" aria-hidden="true"></i> Photo</div>
+                        <input id="uploadNewPost" type="file"  name="image" id="image" class=" @error('image') is-invalid @enderror" autocomplete="image" autofocus>
+                        @error('image')
+                        <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-md-8 offset-md-4">
+                    <img id="img_output" src="" alt="" width="400px" height="400px">
+                </div>
+                <div class="row pl-3 mt-2">
+                    <button class="btn btn-primary col-md-3 offset-md-4 ">Add New Post</button>
+                </div>
+            </div>
         </div>
     </div>
 </form>
 @endsection
 @section('js')
-    <script>
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                console.log(input.files[0]);
-                console.log(input.files);
-                var reader = new FileReader();
-                
-                reader.onload = function(e) {
-                $('#img_output').attr('src', e.target.result);
-                }
-                
-                reader.readAsDataURL(input.files[0]);
+<script>
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            console.log(input.files[0]);
+            console.log(input.files);
+            var reader = new FileReader();
+            
+            reader.onload = function(e) {
+            $('#img_output').attr('src', e.target.result);
             }
+            
+            reader.readAsDataURL(input.files[0]);
         }
-           
-         $(document).on('change','#uploadNewPost',function(){
-                readURL(this);
-            })
-          
-           
-    </script>
+    }
+       
+     $(document).on('change','#uploadNewPost',function(){
+            readURL(this);
+        })
+      
+       
+</script>
 @endsection
