d6d2da2a606f5020efd35ffdefa07b0956e78231
Dam Nguyen <kubombo0007@gmail.com>
2020-05-13 18:27:29 +0700

    fix bug chartjs

--- resources/views/admin/chartpost.blade.php
+++ resources/views/admin/chartpost.blade.php
@@ -1,3 +1,12 @@
+<style>
+    .justify-content{
+       padding-left: 30%;
+        color: #4aa0e6;
+    }
+
+</style>
+
 <div class="card-body">
     <canvas id="myAreaChart" width="100%" height="30"></canvas>
+    <p  class="justify-content">Biểu đồ thống kê bài viết theo tháng</p>
 </div>
