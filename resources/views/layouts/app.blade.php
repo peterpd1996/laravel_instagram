083df78ca3c7dcbbca34cecf4a8c03a26db34526
peterpd1996 <tungduongpd1996@gmail.com>
2020-05-29 23:58:01 +0700

    edit css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Tung Duong</title>
+    <title>D'look</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
      <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="icon" type="image/png" sizes="96x96" href="/images/paper-plane.png">
